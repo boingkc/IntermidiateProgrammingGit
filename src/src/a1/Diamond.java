213930d38ef7b26ce57b6b2b2e01810483382c6f
boingkc <boing191@gmail.com>
2017-08-24 21:02:50 +0700

    progress for A1.Diamond

--- src/src/a1/Diamond.java
+++ src/src/a1/Diamond.java
@@ -1,7 +1,40 @@
 package a1;
+import java.util.*;
+
 
 /**
  * Created by BOINGKC on 24/08/2017 AD.
  */
 public class Diamond {
+    public static void printDiamond(int k){
+        StringBuffer reverse = new StringBuffer();
+        int star = 1;
+
+        while (k >= 1){
+            for (int i = 0; i < k; i++) {
+                reverse.append("#");
+            }
+
+            for (int i = 0; i < star ; i++) {
+                reverse.append("*");
+            }
+
+            for (int i = 0; i < k; i++) {
+                reverse.append("#");
+            }
+
+            reverse.append("\n");
+            k--;
+            star+=2;
+        }
+
+        System.out.println(reverse);
+        System.out.println(reverse.reverse());
+
+
+    }
+
+    public static void main(String[] args) {
+        printDiamond(4);
+    }
 }
